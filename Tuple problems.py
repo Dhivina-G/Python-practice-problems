aee36babfa899c0478c8f7590ac6a4fc1b07cbbb
Dhivina <dhivinagiji@gmail.com>
2024-05-27 17:53:16 +0530

    tuple problems code updated

--- Tuple problems.py
+++ Tuple problems.py
@@ -12,6 +12,14 @@ print(count)
 
 # 27. Python – Maximum and Minimum K elements in Tuple
 
+elements = (1,4,3,12,2,66)
+k = int(input('Enter the number of values to check:'))
+elements_sorted = sorted(elements)                         # sort the elements
+min_values = elements_sorted[:k]                           # using slice get first k elements
+max_values = elements_sorted[-k:]                          # using slice get last k elements
+print(f'minimum values:{min_values}')
+print(f'maximum values:{max_values}')
+
 # 28. Create a list of tuples from given list having number and its cube in each tuple
 
 numbers = [1,2,3,4,5]
@@ -37,3 +45,16 @@ print(items_tuple)
 
 # 30. Python – Closest Pair to Kth index element in Tuple
 
+elements = (1,4,5,6,2)
+k = int(input('Enter the index of element:'))
+k_value = elements[k]                                     # index value of k
+for num in range(len(elements)):                          # iterate from first value
+    for nums in range(num+1,len(elements)):               # iterate from second value
+        if(elements[num]+elements[nums]==k_value):        # check if the sum is equal to k_value
+            print(f'{elements[num]},{elements[nums]}')
+            break
+          
+
+
+
+        
