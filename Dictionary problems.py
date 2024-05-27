bb7605995f08114d2cd6aaed812cba2be6ec6ec6
Dhivina <dhivinagiji@gmail.com>
2024-05-27 14:13:03 +0530

    code updated

--- Dictionary problems.py
+++ Dictionary problems.py
@@ -8,20 +8,26 @@ for details in address_details.values():
 # 22. Python program to find the sum of all items in a dictionary
 
 numbers = {'one':2,'two':4,'three':29,'four':12}
-sum = 0
-for value in numbers.values():   
-    sum += value               
+sum = 0                                    # set sum as zero
+for value in numbers.values():             # get the values in the dictionary]
+    sum += value                           # sum the values
 print(sum)
 
 
 # 23. Python | Ways to remove a key from dictionary
 
 address_details = {'name':'Mark','place':'Delhi','age':29}
-del address_details['name']
+del address_details['name']                                # delete using del
 print(address_details)
 
 
 # 24. Ways to sort list of dictionaries by values in Python – Using itemgetter
+
+from operator import itemgetter                                             # imported itemgetter 
+
+address_details = [{'name':'Mark','place':'Delhi','age':29},{'name':'Serin','place':'Mumbai','age':31},{'name':'John','place':'Kerala','age':41}]
+sorted_address = sorted(address_details,key=itemgetter('age'))               # sorted the dictionary using itemgettr as key 
+print(sorted_address)
 
 # 25. Ways to sort list of dictionaries by values in Python – Using lambda function
 
